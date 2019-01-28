6d75671c687ef7bda95aeaf8de8eab984a52d042
Sujay Purayil <40204472@sg.yokogawa.com>
2019-01-28 16:25:14 +0800

    react added

---
+++ bittergourd/moisture_info.js
@@ -0,0 +1,19 @@
+
+class MoistureInfo extends React.Component {
+
+    render() {
+      return <div>  <h1 style={{color:'blue'}}>{this.props.msg}   <a style={{fontSize:20,color:'black'}}>{new Date().toLocaleTimeString()}</a></h1>   </div>
+    }
+  }
+  
+  
+  function tick() {  
+    // const domContainer = document.querySelector('#moisture_textarea_container');
+    // ReactDOM.render(e(SoilConditionText, { msg: 'Moisture is good' }, null), domContainer);
+    ReactDOM.render(
+      <MoistureInfo msg='Moisture is good'/>, 
+      document.getElementById("moisture_textarea_container")
+  );
+  }
+  
+  setInterval(tick, 1000);
